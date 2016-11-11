7901eb0654074fc399543981a67e08ec7cb1ea59
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2016-11-11 13:41:46 -0800

    Added RSpec gem & model tests

---
+++ spec/models/search_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe SearchDatum, :type => :model do
+  it "is valid with valid attributes" do
+    expect(SearchDatum.new(id: 1, value: 'football', user_id: 1, created_at: Time.current,
+    	updated_at: Time.current)).to be_valid
+  end
+
+  it "is not valid without a title" do
+  	expect(SearchDatum.new(id: 1, value: nil, user_id: 1, created_at: Time.current,
+    	updated_at: Time.current)).to_not be_valid
+  end
+end
