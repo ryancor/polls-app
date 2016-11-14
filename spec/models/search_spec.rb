a04e2afb7c34c932273ef1c063119d319d6209c4
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2016-11-14 10:23:42 -0800

    New method to search_method_spec

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -10,4 +10,10 @@ RSpec.describe SearchDatum, :type => :model do
   	expect(SearchDatum.new(id: 1, value: nil, user_id: 1, created_at: Time.current,
     	updated_at: Time.current)).to_not be_valid
   end
+
+  it "exists when searching" do
+  	search = SearchDatum.new(id: 1, value: 'ruby', user_id: 1, created_at: Time.current,
+    	updated_at: Time.current)
+  	expect(SearchDatum.search_exists(search.value)).to be(false)
+  end
 end
