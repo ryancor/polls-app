0b5b757c29cd980967946571c6b36e66ed453497
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2016-11-14 15:39:31 -0800

    API spec for show & title change

--- spec/controllers/polls_controller_spec.rb
+++ spec/controllers/polls_controller_spec.rb
@@ -9,13 +9,25 @@ RSpec.describe PollsController, :type => :controller do
     end
   end
 
+  describe "GET show" do
+    before do
+      get :show, id: 1
+    end
+
+    Poll.create(topic: 'New title')
+    
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+  end
+
   describe "POST create/:id & update/:id" do
   	it "creates a poll" do
 	   poll_attributes = FactoryGirl.attributes_for(:poll)
-  	   post :create, post: poll_attributes
-  	   response.should redirect_to(root_path)
-  	   Poll.last.topic.should == poll_attributes[:topic]
-	end
+  	 post :create, post: poll_attributes
+  	 response.should redirect_to(root_path)
+  	 Poll.last.topic.should == poll_attributes[:topic]
+	  end
   end
 end
 
