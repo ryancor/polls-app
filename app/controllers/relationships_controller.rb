05e73b8c2f738a78610d9e9d3b641d871e15c853
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2016-12-18 11:24:56 -0800

    Created relationships && privacy

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,8 +1,8 @@
 class RelationshipsController < ApplicationController
 	def create
-  		@relationship = Relationship.create(follower_id: current_user, followed_id: @user)
-  		if @relationship.save
-  			flash[:success] = 'Relationship was created!'
+		@user = User.find(params[:followed_id])
+  		if @relationship = Relationship.create(follower_id: current_user.id, followed_id: @user.id)
+  			flash[:success] = @relationship.follow_who?
   			redirect_to root_path
 		else
 			flash[:warning] = 'Something went wrong!'
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,8 @@ class Relationship < ApplicationRecord
   belongs_to :followed, class_name: "User"
   validates :follower_id, presence: true
   validates :followed_id, presence: true
+
+  def follow_who?
+  	return "#{follower.name} followed #{followed.name}"
+  end
 end
