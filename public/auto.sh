62f192edd91fc01e1059701808c66c4d83adf20f
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2016-12-22 16:28:46 -0800

    Testing auto commits

--- public/auto.sh
+++ public/auto.sh
@@ -1,5 +1,9 @@
 git status
 read -r -p 'Please enter your commit message >>> ' var
-git add .
-git commit -m "$var"
-git push origin master
+if [ $var == 'Exit' ]; then
+	exit 1
+else
+	git add .
+	git commit -m "$var"
+	git push origin master
+fi
