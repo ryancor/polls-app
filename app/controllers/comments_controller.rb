7a9c1af3cd102b529f0331fdb3c13479d4528bdc
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2017-02-05 16:34:24 -0800

    Comments being recorded after POST

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,12 @@
 class CommentsController < ApplicationController
 	def create_comment
+		@comment = Comment.create(body: params[:body], 
+			user_id: current_user.id, poll_id: params[:id])
+		if @comment.save
+			flash[:success] = "You just posted a comment"
+			redirect_to polls_path
+		else
+			render new
+		end
 	end
 end
